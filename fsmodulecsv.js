ca497d9d801b05ec1e3e905484afd7ba04251813
priyas <PriyadharshiniS@hexaware.com>
2020-06-04 15:46:40 +0530

    added

--- fsmodulecsv.js
+++ fsmodulecsv.js
@@ -83,8 +83,8 @@ app.post("/changeName", function (request, response) {
     }
     response.send(data);
 });
-var bookingHistory=[];
-var cardResponse=[];
+//var bookingHistory=[];
+//var cardResponse=[];
 app.post("/fulfillment",function(request, response){
     const intent = request.body.queryResult.intent.displayName;
     logger.info("the request param", JSON.stringify(intent));
@@ -111,104 +111,97 @@ app.post("/fulfillment",function(request, response){
     
     response.send(responseJson)
 
-} else if(intent =='RoomBooking'){
-   var responseJson4={
+//} else if(intent =='RoomBooking'){
+  // var responseJson4={
 
-   } 
-   responseJson4.fulfillmentMessages=[
-    {
-      "quickReplies": {
-        "title": "do you want me to show some rooms?",
-        "quickReplies": [
-          "yes",
-          "no"
-        ]
-      },
-      "platform": "FACEBOOK"
-    },
-    {
-      "text": {
-        "text": [
-          ""
-        ]
-      }
-    }
-  ]
-  response.send(responseJson4);
+   //} 
+   //responseJson4.fulfillmentMessages=[
+    //{
+      //"quickReplies": {
+        //"title": "do you want me to show some rooms?",
+        //"quickReplies": [
+          //"yes",
+          //"no"
+        //]
+      //},
+      //"platform": "FACEBOOK"
+    //},
+    //{
+      //"text": {
+        //"text": [
+          //""
+        //]
+      //}
+    //}
+  //]
+  //response.send(responseJson4);
 }
-else if(intent =='Room types'){
+else if(intent =='shopdisplay'){
     var responseJson3 ={
 
     }
     responseJson3.fulfillmentMessages=[{
-        "card": {
-          "title": "room accomdation",
-          "subtitle": "single bed",
-          "imageUri": "https://setupmyhotel.com/images/Room-Type-Single-Room.jpg",
-          "buttons": [
-            {
-              "text": "book",
-              "postback": "single bed"
-            }
-          ]
+          "platform": "ACTIONS_ON_GOOGLE",
+          "basicCard": {
+            "title": "test1",
+            "subtitle": "test2",
+            "formattedText": "map",
+            "image": {
+              "imageUri": "https://images.news18.com/ibnlive/uploads/2019/05/Google-Maps.jpg?impolicy=website&width=536&height=356",
+              "accessibilityText": "map image"
+            },
+            "buttons": [
+              {
+                "title": "click here",
+                "openUriAction": {
+                  "uri": "http://maps.google.co.in"
+                }
+              }
+            ]
+          }
         },
-        "platform": "FACEBOOK"
-      },
-      {
-        "card": {
-          "title": "room accomdation",
-          "subtitle": "double bed",
-          "imageUri": "https://decorpad.files.wordpress.com/2019/03/big-bedroom-with-two-beds-three-animated-double-bed-small-bedroom-with-double-bed-small-rooms-bedrooms-room-frames.jpg?w=1024",
-          "buttons": [
-            {
-              "text": "book",
-              "postback": "double bed"
-            }
-          ]
-        },
-        "platform": "FACEBOOK"
-      },
-      {
-        "text": {
-          "text": [
-            ""
-          ]
+        {
+          "text": {
+            "text": [
+              ""
+            ]
+          }
         }
-      }]
+      ]
       response.send(responseJson3);
-    
-
 }
- else if(intent == 'Room types Booking - custom')
- { 
-    logger.info( JSON.stringify((request.body)));
-    var ch ='HS';
-   var number = ch.concat(Math.random().toString(36).substring(7));
-     logger.info("congrats!!!ROOMBOOKING id:",number);
-    var random_num = number;
-    var str1="congrats your booking id is:"
-    var result =str1.concat(random_num);
-    var ra=request.body.queryResult.parameters["roomaccomdation"];
-    var rt=request.body.queryResult.parameters["roomtype"];
-    var date =request.body.queryResult.parameters["date"];
-    var formattedDate = moment(date).format('DD-MM-YYYY');
-    var final = "bookingId:"+random_num+"\n"+"roomaccomdation:"+ra+"\n"+"roomtype:"+rt+"\n"+"bookingdate:"+formattedDate+"\n"
+
+
+ //else if(intent == 'Room types Booking - custom')
+ //{ 
+   // logger.info( JSON.stringify((request.body)));
+    //var ch ='HS';
+   //var number = ch.concat(Math.random().toString(36).substring(7));
+     //logger.info("congrats!!!ROOMBOOKING id:",number);
+    //var random_num = number;
+    //var str1="congrats your booking id is:"
+    //var result =str1.concat(random_num);
+    //var ra=request.body.queryResult.parameters["roomaccomdation"];
+    //var rt=request.body.queryResult.parameters["roomtype"];
+    //var date =request.body.queryResult.parameters["date"];
+    //var formattedDate = moment(date).format('DD-MM-YYYY');
+    //var final = "bookingId:"+random_num+"\n"+"roomaccomdation:"+ra+"\n"+"roomtype:"+rt+"\n"+"bookingdate:"+formattedDate+"\n"
  
-    bookingHistory.push({
-        bookingId : random_num,
-        roomaccomdation : ra,
-        roomtype: rt,
-        bookingdate : date,
-        class : "Booked"
-    })
+    //bookingHistory.push({
+       // bookingId : random_num,
+        //roomaccomdation : ra,
+        //roomtype: rt,
+        //bookingdate : date,
+        //class : "Booked"
+    //})
 
-  var responseJson1 ={
+  //var responseJson1 ={
 
-  }
+  //}
     
     // bookingHistory.push(final);
-    logger.info(bookingHistory);
-  responseJson1.fulfillmentText=final; 
+    //logger.info(bookingHistory);
+  //responseJson1.fulfillmentText=final; 
   //responseJson1.fulfillmentMessages=[{
     //"card": {
       //  "title": result,
@@ -223,59 +216,59 @@ else if(intent =='Room types'){
       //"platform": "FACEBOOK"
 
 //}]
-  response.send(responseJson1);
-}
-else if(intent == 'orderHistory') {
-    var responseJson2 ={
+  //response.send(responseJson1);
+//}
+//else if(intent == 'orderHistory') {
+   // var responseJson2 ={
 
-    }
-    logger.info(bookingHistory)
-    bookingHistory.forEach(element => { 
+    //}
+    //logger.info(bookingHistory)
+    //bookingHistory.forEach(element => { 
     
-        var singleCard={
-             "card": {
-                 "title": "",
-                 "subtitle": "",
-                 "imageUri": "",
-                 "buttons": [
-                   {
-                     "text": ""
-                   }
-                 ]
-               },
-               "platform": "FACEBOOK"
-         } 
-     singleCard.card.title = element.bookingId;
-     singleCard.card.subtitle = element.roomtype;
-     singleCard.card.buttons[0].text = element.class;
-     singleCard.card.imageUri = "https://setupmyhotel.com/images/Room-Type-Single-Room.jpg";
-     cardResponse.push(singleCard);
+       // var singleCard={
+            // "card": {
+              //   "title": "",
+                // "subtitle": "",
+                // "imageUri": "",
+                 //"buttons": [
+                  // {
+                     //"text": ""
+                   //}
+                 //]
+               //},
+               //"platform": "FACEBOOK"
+         //} 
+     //singleCard.card.title = element.bookingId;
+     //singleCard.card.subtitle = element.roomtype;
+     //singleCard.card.buttons[0].text = element.class;
+     //singleCard.card.imageUri = "https://setupmyhotel.com/images/Room-Type-Single-Room.jpg";
+     //cardResponse.push(singleCard);
  
  
-     })
-     responseJson2.fulfillmentMessages = cardResponse;
-     console.log("Response to User", JSON.stringify(responseJson2));
-      response.send(responseJson2);
-}
-else if(intent =='Default Fallback Intent')
-{
-    var responseJson4={
+     //})
+     //responseJson2.fulfillmentMessages = cardResponse;
+     //console.log("Response to User", JSON.stringify(responseJson2));
+      //response.send(responseJson2);
+//}
+//else if(intent =='Default Fallback Intent')
+//{
+   // var responseJson4={
 
-    } 
-    responseJson4.fulfillmentMessages=  [
-        {
-          "quickReplies": {
-            "title": "sorry!!!i din't get you",
-            "quickReplies": [
-              "wanna book a room?",
-              "book a room?",
-              "need a room?"
-            ]
-          },
-          "platform": "FACEBOOK"
-        }
-      ]
-}
+    //} 
+    //responseJson4.fulfillmentMessages=  [
+      //  {
+        //  "quickReplies": {
+          //  "title": "sorry!!!i din't get you",
+            //"quickReplies": [
+              //"wanna book a room?",
+              //"book a room?",
+              //"need a room?"
+            //]
+          //},
+          //"platform": "FACEBOOK"
+        //}
+      //]
+//}
     
 }); 
 app.listen(process.env.PORT || 8080, function () {
