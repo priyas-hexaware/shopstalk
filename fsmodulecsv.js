8e3153f27d792a2f9e8245ad3b3d358ae703f872
priyas <PriyadharshiniS@hexaware.com>
2020-06-04 21:45:32 +0530

    added

--- fsmodulecsv.js
+++ fsmodulecsv.js
@@ -84,8 +84,8 @@ app.post("/changeName", function (request, response) {
     response.send(data);
 });
 
-//var bookingHistory=[];
-//var cardResponse=[];
+var searchHistory=[];
+var cardResponse=[];
 app.post("/fulfillment",function(request, response){
     const intent = request.body.queryResult.intent.displayName;
     logger.info("the request param", JSON.stringify(intent));
@@ -151,8 +151,17 @@ else if(intent == 'pincode validation')
     }
     else
     {
-      var res ="zipcode invalid"
+      var res ="zipcode invalid.PLEASE ENTER PROPER ZIPCODE!!"
     }
+    var date = request.body.date
+    var formattedDate=moment(date).format('DD-MM-YYYY');
+    searchHistory.push({
+      Zipcode : zipcode,
+      date:formattedDate,
+      class : "searched"
+  })
+
+    
     var responseJson1 ={
 
     }
@@ -204,60 +213,54 @@ else if(intent =='shopdisplay'){
       }
     ]
       response.send(responseJson3);
+  
+
+}
+else if(intent == 'searchhistory') {
+    var responseJson4 ={
+
     }
-
-    //var rt=request.body.queryResult.parameters["roomtype"];
-    //var date =request.body.queryResult.parameters["date"];
-    //var formattedDate = moment(date).format('DD-MM-YYYY');
-    //var final = "bookingId:"+random_num+"\n"+"roomaccomdation:"+ra+"\n"+"roomtype:"+rt+"\n"+"bookingdate:"+formattedDate+"\n"
- 
-    //bookingHistory.push({
-       // bookingId : random_num,
-        //roomaccomdation : ra,
-        //roomtype: rt,
-        //bookingdate : date,
-        //class : "Booked"
-    //})
-
+    logger.info(searchHistory)
+    searchHistory.forEach(element => { 
+        var singleCard={
+  
+        
+        
+          "platform": "ACTIONS_ON_GOOGLE",
+          
+          "basicCard": {
+            "title": "",
+            "subtitle": "",
+            "formattedText": "map",
+            "image": {
+              "imageUri": "",
+              "accessibilityText": "map image"
+            },
+            "buttons": [
+              {
+                "title": "",
+                "openUriAction": {
+                  "uri": "http://maps.google.co.in"
+                }
+              }
+            ]
+          }
+        }
+      
+        singleCard.basicCard.title = element.Zipcode ;
+        singleCard.basicCard.subtitle = element.date;
+        singleCard.basicCard.buttons[0].title= element.class;
+        singleCard.basicCard.imageUri = "https://images.news18.com/ibnlive/uploads/2019/05/Google-Maps.jpg?impolicy=website&width=536&height=356";
+        cardResponse.push(singleCard);
     
-    // bookingHistory.push(final);
-    //logger.info(bookingHistory);
-  //responseJson1.fulfillmentMessages=[{
-    //"card": {
-      //  "title": result,
-        //"subtitle": rt,
-        //"imageUri": "https://lumiere-a.akamaihd.net/v1/images/rich_mobile_thejunglebook_header_poststreet_46b1223f.jpeg?region=0,0,640,600",
-        //"buttons": [
-         // {
-         //   "text": "book"
-         // }
-        //]
-     // },
-      //"platform": "FACEBOOK"
-
-//}]
-
-//}
-//else if(intent == 'orderHistory') {
-   // var responseJson2 ={
-
-    //}
-    //logger.info(bookingHistory)
-    //bookingHistory.forEach(element => { 
     
-       // var singleCard={
-            // "card": {
-              //   "title": "",
-                // "subtitle": "",
-                // "imageUri": "",
-                 //"buttons": [
-                  // {
-                     //"text": ""
-                   //}
-                 //]
-               //},
-               //"platform": "FACEBOOK"
-         //} 
+        })
+        responseJson4.fulfillmentMessages = cardResponse;
+        console.log("Response to User", JSON.stringify(responseJson4));
+         response.send(responseJson4);
+   }
+  
+    
      //singleCard.card.title = element.bookingId;
      //singleCard.card.subtitle = element.roomtype;
      //singleCard.card.buttons[0].text = element.class;
